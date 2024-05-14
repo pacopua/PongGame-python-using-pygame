8958afb9714484353542b9a2e009ad3491ec1d71
pacopua <113210040+pacopua@users.noreply.github.com>
2024-05-14 17:22:56 +0200

    Add files via upload

---
+++ proyecto/pruebas.py
@@ -0,0 +1,14 @@
+import pygame
+pygame.init()
+dis=pygame.display.set_mode((800,600))
+pygame.display.update()
+pygame.display.set_caption('Snake game by Edureka')
+game_over=False
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+        print(event)   #prints out all the actions that take place on the screen
+ 
+pygame.quit()
+quit()
